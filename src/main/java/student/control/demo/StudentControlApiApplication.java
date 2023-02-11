9551649c3acacf39f86e659770abd05806e00743
Andrew Silva <andrew.silva@bemobi.com>
2023-02-11 18:48:27 -0300

    Arquivos iniciais do projeto.

---
+++ src/main/java/student/control/demo/StudentControlApiApplication.java
@@ -0,0 +1,13 @@
+package student.control.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/student/control/demo/StudentControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package student.control.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
