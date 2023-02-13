f64e82f50a3131b40134e215dec4bdaa0a3e2024
AndrewRSS <andrewrss15@gmail.com>
2023-02-12 21:58:18 -0300

    fix name

---
+++ src/main/java/student/controlAPI/ControlApiApplication.java
@@ -0,0 +1,13 @@
+package student.controlAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/student/controlAPI/ControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package student.controlAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
